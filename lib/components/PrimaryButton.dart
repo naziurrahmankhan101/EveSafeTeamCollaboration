c3b4c35a3af92708b58470fcd69beee24707897d
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2023-12-21 23:09:25 +0600

    Login problem debug

--- lib/components/PrimaryButton.dart
+++ lib/components/PrimaryButton.dart
@@ -2,41 +2,33 @@ import 'package:after_marjana/utils/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PrimaryButton extends StatelessWidget{
+
+
+class PrimaryButton extends StatelessWidget {
   final String title;
   final Function onPressed;
   bool loading;
   PrimaryButton(
-      {required this.title,
-      required this.onPressed,
-      this.loading=false}
-      );
+      {required this.title, required this.onPressed, this.loading = false});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 60,
       width: double.infinity,
       child: ElevatedButton(
-          onPressed: () {
-             onPressed();
-          }, child: Text(
+        onPressed: () {
+          onPressed();
+        },
+        child: Text(
           title,
-      style: TextStyle(fontSize: 18),
-
-      ),
-
-        style: ElevatedButton.styleFrom(
-          backgroundColor: primaryColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30)
-          ),
-
+          style: TextStyle(fontSize: 18),
         ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30))),
       ),
-
     );
-    // TODO: implement build
-    throw UnimplementedError();
   }
-
 }
--- lib/components/SecondaryButton.dart
+++ lib/components/SecondaryButton.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 
 class SecondaryButton extends StatelessWidget {
-   final String title;
-   final Function onPressed;
-  const SecondaryButton({Key? key,required this.title, required this.onPressed}) : super(key: key);
+  final String title;
+  final Function onPressed;
+
+  const SecondaryButton(
+      {Key? key, required this.title, required this.onPressed})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-  return Container(
-
-    child: TextButton(
-      onPressed: (){
-        onPressed();
-      },
-      child: Text(
-        title,
-        style: TextStyle(fontSize: 18),
-
-      )
-    ),
-  );
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      child: TextButton(
+          onPressed: () {
+            onPressed();
+          },
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 18),
+          )),
+    );
   }
 }
