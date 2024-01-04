a804bfcc5d3e00fa90fc2f5e0729d4e48bcc2a11
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-01-05 03:24:22 +0600

    test trial

--- lib/child/bottom_screens/contacts_page.dart
+++ lib/child/bottom_screens/contacts_page.dart
@@ -1,12 +1,56 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
-class ContactsPage extends StatelessWidget {
+import 'package:after_marjana/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ContactsPage extends StatefulWidget {
   const ContactsPage({super.key});
 
   @override
+  State<ContactsPage> createState() => _ContactsPageState();
+}
+
+class _ContactsPageState extends State<ContactsPage> {
+
+  @override
+  void initState(){
+    super.initState();
+    askPermissions();
+  }
+
+
+  Future<void> askPermissions() async{
+    PermissionStatus permissionStatus= await getContactsPermissions();
+
+    if(permissionStatus==PermissionStatus.granted){
+
+    }else{
+      handInvalidPermission(permissionStatus);
+    }
+  }
+
+  handInvalidPermission(PermissionStatus permissionStatus){
+    if(permissionStatus==PermissionStatus.denied){
+      dialogueBox(context, "Access is denied");
+    }else if(permissionStatus == PermissionStatus.permanentlyDenied){
+      dialogueBox(context, "Contact doesn't exist");
+    }
+  }
+
+  Future<PermissionStatus> getContactsPermissions() async{
+    PermissionStatus permission= await Permission.contacts.status;
+    if(permission != PermissionStatus.granted &&
+        permission != PermissionStatus.permanentlyDenied) {
+      PermissionStatus permissionStatus = await Permission.contacts.request();
+      return permissionStatus;
+    }else
+      return permission;
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Center(child: Text("Contact Page")),
+      body: Center(child: Text("Contact Page")),
     );
   }
 }
