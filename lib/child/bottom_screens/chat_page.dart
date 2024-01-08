0aae74a513f360ae4310b795325bd044dbfe1bf9
Marjana026 <marjana.salauddin@gmail.com>
2024-01-08 23:57:46 +0600

    worked on adding guardian in chatbox option

--- lib/child/bottom_screens/chat_page.dart
+++ lib/child/bottom_screens/chat_page.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/constants.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -7,11 +11,40 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar : AppBar(
+          backgroundColor: Colors.pink,
           title: Text("SELECT GUARDIAN"),
         ),
-      body: Column(
-        children: [],
-      )
+      body:
+      StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .where('type', isEqualTo: 'parent')
+              .where('childEmail',isEqualTo: FirebaseAuth.instance.currentUser!.email)
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot <QuerySnapshot> snapshot){
+            if(!snapshot.hasData){
+              return Center(child: progressIndicator(context));
+            }
+
+             return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                 itemBuilder: (BuildContext context,int index){
+                  final d = snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Color.fromARGB(255, 250, 163, 192),
+                      child: ListTile(
+                        title: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(d['name']),
+                        ),
+                      ),
+                    ),
+                  );
+                 },);
+          },
+      ),
     );
   }
 }
