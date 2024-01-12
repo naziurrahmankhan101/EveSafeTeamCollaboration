04fa5c43d7801d3d70c8ecd7a0d3bb345f96ee2f
Marjana026 <marjana.salauddin@gmail.com>
2024-01-13 00:19:14 +0600

    Parent home screen added

--- lib/parent/parent_home_screen.dart
+++ lib/parent/parent_home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
 
 class ParentHomeScreen extends StatelessWidget {
   const ParentHomeScreen({super.key});
@@ -7,9 +11,40 @@ class ParentHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('parent screen'),
-      )
-    );
+        appBar : AppBar(
+          backgroundColor: Colors.pink,
+          title: Text("SELECT CHILD"),
+        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('type', isEqualTo: 'child')
+            .where('guardiantEmail',isEqualTo: FirebaseAuth.instance.currentUser!.email)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot <QuerySnapshot> snapshot){
+          if(!snapshot.hasData){
+            return Center(child: progressIndicator(context));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context,int index){
+              final d = snapshot.data!.docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Color.fromARGB(255, 250, 163, 192),
+                  child: ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(d['name']),
+                    ),
+                  ),
+                ),
+              );
+            },);
+        },
+      ),
+         );
   }
 }
