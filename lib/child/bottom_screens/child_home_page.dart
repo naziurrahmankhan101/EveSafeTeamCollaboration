3e1d43a9cdea86dfaf5ffe80e62c03d568e59335
Marjana026 <marjana.salauddin@gmail.com>
2024-01-26 17:17:11 +0600

    added SHAKE option to share location to guardian

--- lib/child/bottom_screens/child_home_page.dart
+++ lib/child/bottom_screens/child_home_page.dart
@@ -1,13 +1,22 @@
 import 'dart:math';
 
-
-import 'package:after_marjana/widgets/home_widgets/emergency.dart';
-import 'package:after_marjana/widgets/home_widgets/live_safe.dart';
-import 'package:after_marjana/widgets/home_widgets/safehome/SafeHome.dart';
+import 'package:background_sms/background_sms.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shake/shake.dart';
+import 'package:telephony/telephony.dart';
+import 'package:after_marjana/db/db_services.dart';
+import 'package:after_marjana/model/contactsm.dart';
+import 'package:after_marjana/widgets/home_widgets/CustomCarouel.dart';
 import 'package:after_marjana/widgets/home_widgets/custom_appBar.dart';
+import 'package:after_marjana/widgets/home_widgets/emergency.dart';
+import 'package:after_marjana/widgets/home_widgets/safehome/SafeHome.dart';
 
-import '../../widgets/home_widgets/CustomCarouel.dart';
+import '../../widgets/home_widgets/live_safe.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -15,87 +24,193 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-//  const HomeScreen({super.key});
-  int qindex =0;
+  // const HomeScreen({super.key});
+  int qIndex = 0;
+  Position? _curentPosition;
+  String? _curentAddress;
+  LocationPermission? permission;
+  _getPermission() async => await [Permission.sms].request();
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+  // _sendSms(String phoneNumber, String message, {int? simSlot}) async {
+  //   SmsStatus result = await BackgroundSms.sendMessage(
+  //       phoneNumber: phoneNumber, message: message, simSlot: 1);
+  //   if (result == SmsStatus.sent) {
+  //     print("Sent");
+  //     Fluttertoast.showToast(msg: "send");
+  //   } else {
+  //     Fluttertoast.showToast(msg: "failed");
+  //   }
+  // }
 
-  getRandomQuote(){
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  _getCurrentLocation() async {
+    final hasPermission = await _handleLocationPermission();
+    final Telephony telephony = Telephony.instance;
+    await telephony.requestPhoneAndSmsPermissions;
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+        forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _curentPosition = position;
+        print(_curentPosition!.latitude);
+        _getAddressFromLatLon();
+      });
+    }).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+    });
+  }
+
+  _getAddressFromLatLon() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _curentPosition!.latitude, _curentPosition!.longitude);
+
+      Placemark place = placemarks[0];
+      setState(() {
+        _curentAddress =
+        "${place.locality},${place.postalCode},${place.street},";
+      });
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
+
+  getRandomQuote() {
     Random random = Random();
     setState(() {
-      qindex=random.nextInt(6);
+      qIndex = random.nextInt(6);
     });
+  }
 
+  getAndSendSms() async {
+    List<TContact> contactList = await DatabaseHelper().getContactList();
+
+    String messageBody =
+        "https://maps.google.com/?daddr=${_curentPosition!.latitude},${_curentPosition!.longitude}";
+    if (await _isPermissionGranted()) {
+      contactList.forEach((element) {
+        // _sendSms("${element.number}", "i am in trouble $messageBody");
+      });
+    } else {
+      Fluttertoast.showToast(msg: "something wrong");
+    }
   }
 
   @override
   void initState() {
     getRandomQuote();
     super.initState();
+    _getPermission();
+    _getCurrentLocation();
+
+    ////// shake feature ///
+
+    // To close: detector.stopListening();
+    // ShakeDetector.waitForStart() waits for user to call detector.startListening();
   }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.white,
       body: SafeArea(
-
         child: Padding(
-          padding: const EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              /*CustomAppBar(
-                quoteIndex: qindex,
-                onTap: getRandomQuote(),
-              ),*/
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    //width: MediaQuery.of(context).size.width,
-                    child: Transform.scale(
-                      scale: 3,
-                      child: Image.asset(
-                        'assets/evesafe.png',
-                        height: 40,
-                        width: 40,
-                      ),
-                    ),
-                  ),
-                  // Add more widgets as needed
-                ],
+              SizedBox(
+                height: 10,
+                child: Container(
+                  color: Colors.grey.shade100,
+                ),
+              ),
+              SizedBox(height: 5),
+              CustomAppBar(
+                  quoteIndex: qIndex,
+                  onTap: () {
+                    getRandomQuote();
+                  }),
+              SizedBox(height: 5),
+              SizedBox(
+                height: 10,
+                child: Container(
+                  color: Colors.grey.shade100,
+                ),
               ),
               Expanded(
                 child: ListView(
                   shrinkWrap: true,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        "     ",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
+                  children: [
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Incase of emergency dial me",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
-                    CustomAppBar(
-                      quoteIndex: qindex,
-                      onTap: getRandomQuote(),
-                    ),
-                    CustomCarouel(),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-
-                      child: Text("Emergency",
-                        style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.bold),
-                      ),
-                    ),
-
                     Emergency(),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text("Explore LiveSafe",
-                        style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.bold),
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Explore your power",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
+                    SizedBox(height: 10),
+                    CustomCarouel(),
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Explore LiveSafe",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
                     LiveSafe(),
                     SafeHome(),
                   ],
@@ -108,4 +223,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
